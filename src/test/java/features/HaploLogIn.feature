4426fc2197deecb35e808a8d2853b29142eb5e2a
Nguyen Thi Thu Hien <nguyenhien26051994@gmail.com>
2020-07-30 17:00:27 +0700

    Add more failed case 1

--- src/test/java/features/HaploLogIn.feature
+++ src/test/java/features/HaploLogIn.feature
@@ -1,7 +1,7 @@
 Feature: Login611
 
   @appium
-  Scenario Outline: Login failed with invalid username and password - account 1
+  Scenario Outline: Login failed with invalid username and password - case 1
     When I click next button
     And I click skip intro
     And I input email "<email>"
@@ -14,7 +14,7 @@ Feature: Login611
       | hnhaplo@ | 1234gm1     | Email is invalid | Password must be minimum 8 characters |
 
   @appium
-  Scenario Outline: Login failed with invalid username and password - account 2
+  Scenario Outline: Login failed with invalid username and password - case 2
     When I click next button
     And I click skip intro
     And I input email "<email>"
@@ -28,7 +28,45 @@ Feature: Login611
       | hnhaplo+02@gmail.com  | 12345678     | User not found        |
 
   @appium
-  Scenario Outline: Login failed with invalid username and password - account 3
+  Scenario Outline: Forgot password - case 3
+    When I click next button
+    And I click skip intro
+    And I click forgot password
+    And I enter email "<emailForgot>"
+    Then The error message for invalid email "<errorForgot>" is displayed
+
+    Examples:
+      | emailForgot                      |errorForgot|
+      | hnhaplo                          |Email is invalid|
+
+  @appium
+  Scenario Outline: Forgot password - case 4
+    When I click next button
+    And I click skip intro
+    And I click forgot password
+    And I enter email "<emailForgot>"
+    And I click Next button in forgot password
+    Then The snackbar error message for invalid email "<errorForgotSnackbar>" is displayed
+
+    Examples:
+      | emailForgot                      | errorForgotSnackbar    |
+      | hnhaplo+100@gmail.com             | Email is invalid       |
+
+  @appium
+  Scenario Outline: Forgot password - case 5
+    When I click next button
+    And I click skip intro
+    And I click forgot password
+    And I enter email "<emailForgot>"
+    And I click Next button in forgot password
+    Then I click Back login
+
+    Examples:
+      | emailForgot         |
+      | hnhaplo+4@gmail.com |
+
+  @appium
+  Scenario Outline: Login successfully - case 6
     When I click next button
     And I click skip intro
     And I input email "<email>"
--- src/test/java/stepdefinations/HaploLogin.java
+++ src/test/java/stepdefinations/HaploLogin.java
@@ -9,6 +9,8 @@ import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import utility.Hook;
 
+import java.util.concurrent.TimeUnit;
+
 public class HaploLogin {
     private WebDriver driver;
 
@@ -34,7 +36,6 @@ public class HaploLogin {
     public void iInputEmail(String email) throws Throwable {
         driver.findElement(By.id("com.northgate.haplo.dev:id/edtEmail")).clear();
         driver.findElement(By.id("com.northgate.haplo.dev:id/edtEmail")).sendKeys(email);
-        driver.navigate().back();
 
     }
 
@@ -63,6 +64,48 @@ public class HaploLogin {
 
     @And("^The error message snackbar \"([^\"]*)\" is displayed$")
     public void theErrorMessagePromptIsDisplayed(String error) throws Throwable {
-        driver.findElement(By.id("com.northgate.haplo.dev:id/snacksbar_text")).isDisplayed();
+        try{
+            driver.findElement(By.id("com.northgate.haplo.dev:id/snacksbar_text")).isDisplayed();
+        }
+        catch(Exception e){
+        }
+    }
+
+    //Forgot password
+    @And("^I click forgot password$")
+    public void iClickForgotPassword() {
+        driver.findElement(By.id("com.northgate.haplo.dev:id/textView7")).click();
+    }
+
+    @And("^I enter email \"([^\"]*)\"$")
+    public void iEnterEmail(String emailForgot) throws Throwable {
+//        driver.findElement(By.id("com.northgate.haplo.dev:id/edtEmail")).clear();
+//        driver.findElement(By.id("com.northgate.haplo.dev:id/edtEmail")).sendKeys(emailForgot);
+        driver.findElement(By.xpath("//android.widget.EditText[@bounds='[32,277][688,365]']")).sendKeys(emailForgot);
+//        driver.navigate().back();
+    }
+
+    @Then("^The error message for invalid email \"([^\"]*)\" is displayed$")
+    public void theErrorMessageForInvalidEmailIsDisplayed(String errorForgot) throws Throwable {
+        driver.findElement(By.id("com.northgate.haplo.dev:id/textinput_error")).isDisplayed();
+    }
+
+    @Then("^The snackbar error message for invalid email \"([^\"]*)\" is displayed$")
+    public void theSnackbarErrorMessageForInvalidEmailIsDisplayed(String errorForgotSnackbar) throws Throwable {
+        try {
+            driver.findElement(By.id("com.northgate.haplo.dev:id/snackbar_text")).isDisplayed();
+        }
+        catch (Exception e){
+        }
+    }
+
+    @And("^I click Next button in forgot password$")
+    public void iClickNextButtonInForgotPassword() {
+        driver.findElement(By.id("com.northgate.haplo.dev:id/tvConfirm")).click();
+    }
+
+    @Then("^I click Back login$")
+    public void iClickBackLogin() {
+        driver.findElement(By.id("com.northgate.haplo.dev:id/tvBackLogin")).click();
     }
 }
